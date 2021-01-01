0acd6307115d7472d29f4001f0e8224fc4937e15
piyushgupta1510 <piyushgupta1510@gmail.com>
2021-01-02 00:34:52 +0100

    first change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> My favourite hobbies</h1>
+
+    <ul>
+      <li> BOOKS </li>
+      <li> SLEEP </li>
+      <li> ENJOY </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
