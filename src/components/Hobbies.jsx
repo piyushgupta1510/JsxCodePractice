81985300f9911b855f304d1e789f759cacd4fd73
piyushgupta1510 <piyushgupta1510@gmail.com>
2021-01-02 22:28:08 +0100

    split in components

---
+++ src/components/Hobbies.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hobbies() {
+  return (
+    <ul>
+      <li>Book</li>
+      <li>Sleep</li>
+      <li>Relax</li>
+    </ul>
+  );
+}
+
+export default Hobbies;
